c951d9262ad98aeec76c605c999e5f197b6ff2d0
Irirwanirira <irijoseph41@gmail.com>
2022-11-29 23:35:13 +0200

    Modified Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getCountries } from "../redux/home/information";
+import { v4 as uuidv4 } from 'uuid';
+
 const Home = () => {
+  const { countries, status }  = useSelector((state) => state.datas)
+  console.log(countries)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCountries())
+  }, [dispatch])
+
   return (
     <div>
-      <h1>Home</h1>
+     {countries.map((item) => (
+      <NavLink state={item} to='/details'>
+        <div key={uuidv4()} >
+        <h1>{item.country}</h1>
+        <p>{item.deaths}</p>
+        </div>
+      </NavLink>
+     
+     ))}
     </div>
-  );
+  )
 };
 
 export default Home;
