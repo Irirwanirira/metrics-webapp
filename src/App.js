d4522615e7ff8fc70397b3866a7a532c6845ab05
Irirwanirira <irijoseph41@gmail.com>
2022-11-29 23:34:24 +0200

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import { BrowserRouter as Router, Route ,Routes  } from 'react-router-dom';
 import './App.css'
 import Details from './components/details';
 import Home from './components/Home';
-import Nav from './components/nav';
 
 function App() {
   return (
     <Router>
     <div>
-      <Nav />
       <div>
       <Routes>
         <Route path='/' element={ <Home />} />
         <Route path='/details' element={<Details />} />
         </Routes>
       </div>
-      
-      
     </div>
     </Router>
-   
   )
-   
 }
 
 export default App;
