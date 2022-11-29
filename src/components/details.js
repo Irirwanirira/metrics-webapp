1ab64945346659a6517a4132333775ba981e7f2b
Irirwanirira <irijoseph41@gmail.com>
2022-11-29 23:33:47 +0200

    Modified details.js

--- src/components/details.js
+++ src/components/details.js
@@ -1,7 +1,20 @@
+import { useLocation } from "react-router-dom";
+
 const Details = () => {
+  const location = useLocation();
+  const {state } =  location;
+
   return (
     <div>
-      <h1> Details </h1>
+      <h1>{state.country}</h1>
+      <p> Continent: {state.continent}</p>
+      <p> Cases: {state.cases}</p>
+      <p> Today's Case: {state.todayCases}</p>
+      <p> Death: {state.deaths}</p>
+      <p> Today's Death: {state.todayDeaths}</p>
+      <p> Recovered: {state.recovered}</p>
+      <p> Today's Recovered: {state.todayRecovered}</p>
+      <p> Tests:  {state.tests}</p>
     </div>
   );
 };
