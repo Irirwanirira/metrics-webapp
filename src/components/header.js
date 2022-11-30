0aa3efe9321cd09b1a8bd3a5e11e1f0c066cba3f
Irirwanirira <irijoseph41@gmail.com>
2022-11-30 14:17:05 +0200

    Modified header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,19 @@
 import {
-  faCircleArrowRight,
+  faCircleArrowLeft,
   faGear,
   faMicrophone,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useSelector } from "react-redux";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Heading = () => {
-  const { back } = useSelector((state) => state.datas);
-
   return (
     <nav className="nav_bar">
-
-<Link to="/">
-        <FontAwesomeIcon icon={faCircleArrowRight} className="arrow-right" />
+      <Link to="/">
+        <FontAwesomeIcon icon={faCircleArrowLeft} id="arrow-left" />
       </Link>
-      {back ? (
-        <NavLink to="/" className="back_to_home">
-          Back
-        </NavLink>
-      ) : (
-        ""
-      )}
       <h2>COVID-19 Updates</h2>
-      <div>
+      <div className="icons">
         <FontAwesomeIcon icon={faMicrophone} className="icon_microphone" />
         <FontAwesomeIcon icon={faGear} className="icon_gear" />
       </div>
