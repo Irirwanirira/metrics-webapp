1ca4f1a2d3926fb9225845d6760f4eaf22525020
Irirwanirira <irijoseph41@gmail.com>
2022-11-30 14:15:18 +0200

    Test the mocked function

---
+++ src/tests/Home.test.js
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import Home from "../components/Home";
+import store from "../redux/configureStore";
+
+
+describe('Missions', () => {
+    it('It has to render the landing page', () => {
+      const home = render(
+        <>
+          <Provider store={store}>
+            <Home />
+          </Provider>
+        </>,
+      );
+      expect(home).toMatchSnapshot();
+    });
+  });
+  
