1e7a8fead912cf7dd402f3645a8a65b3c4650c6f
Irirwanirira <irijoseph41@gmail.com>
2022-11-30 11:49:22 +0200

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route ,Routes  } from 'react-router-dom';
 import './App.css'
 import Details from './components/details';
+import Heading from './components/header';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
     <div>
+      <Heading />
       <div>
       <Routes>
         <Route path='/' element={ <Home />} />
