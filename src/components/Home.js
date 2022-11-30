b2817335d94246211c95d867bd0f02e782e476e2
Irirwanirira <irijoseph41@gmail.com>
2022-11-30 11:48:23 +0200

    Modified components .js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,31 +2,31 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getCountries } from "../redux/home/information";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
-  const { countries, status }  = useSelector((state) => state.datas)
-  console.log(countries)
-
+  const { countries, status } = useSelector((state) => state.datas);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCountries())
-  }, [dispatch])
+    dispatch(getCountries());
+  }, [dispatch]);
 
   return (
-    <div>
-     {countries.map((item) => (
-      <NavLink state={item} to='/details'>
-        <div key={uuidv4()} >
-        <h1>{item.country}</h1>
-        <p>{item.deaths}</p>
-        </div>
-      </NavLink>
-     
-     ))}
+    <div className="display_flex">
+      {countries.map((item) => (
+        <NavLink state={item} to="/details">
+          <div key={uuidv4()} className="country_display">
+          <FontAwesomeIcon icon={faCircleArrowRight} className="arrow-right" />
+            <p>{item.country}</p>
+            <p>{item.updated}</p>
+          </div>
+        </NavLink>
+      ))}
     </div>
-  )
+  );
 };
 
 export default Home;
